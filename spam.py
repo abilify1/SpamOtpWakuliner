9761fbaaadda5d21f6d26b32331769e3bf29b2da
AbilSeno <48641526+AbilSeno@users.noreply.github.com>
2020-09-23 08:36:04 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,12 @@
+import requests, json
+yu = input('Massukkan nomor target (ex:088118) ; ')
+eek = int(input('Massukkan jumlah : '))
+hd  = {'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}
+dat = {'nama':'Abil+Seno','email_register':'abilseno11%40gmail.com','hp':'08811883541','ulang_tahun':'01%2F01%2F1975','pass':'savagetro123','jns_kelamin':'1'}
+for x in range(eek):
+ ho = requests.post('https://www.wakuliner.com/main/reg13',headers=hd,data=dat)
+ kntl = json.loads(ho.text)
+ if kntl["status"] == 1:
+    print("Sukses spam ke no "+yu)
+ else:
+    print("Gagal spam ke no "+yu)
